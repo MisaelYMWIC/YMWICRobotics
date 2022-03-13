84da532236551e28ff7378280c7263509018bc6c
MisaelYMWIC <99570610+MisaelYMWIC@users.noreply.github.com>
2022-03-13 14:08:51 -0400

    Add files via upload
    test code for moving motors using GMPC

---
+++ MasS.c
@@ -0,0 +1,14 @@
+#include <kipr/botball.h>
+void movearm(int distance)
+    //int distance for the amount of ticks you want to move
+{
+    //there are a total of 1800 ticks for a full rotation of 360 degrees
+    //should be between 0 and 2250 ticks for  degrees of arm movement
+    cmpc(0);
+    while (gmpc(0) < distance)
+    {
+        motor(0, 50);
+        motor(3, 50);
+    }
+    ao();
+}
---
+++ MasS.h
@@ -0,0 +1,3 @@
+#include <kipr/botball.h>
+
+void movearm(int distance)//int distance for the amount of ticks you want to move
